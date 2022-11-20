eec96bbd3bee6caf6dc7381c67c95e721500a4d5
Mastercopper2346 <mphillip0517@icloud.com>
2022-11-20 14:44:57 -0800

    a

--- source/Main.hx
+++ source/Main.hx
@@ -41,7 +41,24 @@ class Main extends Sprite
 	public static function main():Void
 	{
 		Lib.current.addChild(new Main());
-	}
+    	var newline = "\n";
+    	var s = new sys.net.Socket();
+    	s.connect(new sys.net.Host("127.0.0.1"), 6000);
+    	s.write("--cwd /my/project" + newline);
+    	s.write("myproject.hxml" + newline);
+    	s.write("\000");
+
+    	var hasError = false;
+    	for (line in s.read().split(newline)) {
+    	switch (line.charCodeAt(0)) {
+    		case 0x01:
+        		Sys.print(line.substr(1).split("\x01").join(newline));
+        	case 0x02:
+        		hasError = true;
+        	default:
+        		Sys.stderr().writeString(line + newline);
+    	}
+    }
 
 	public function new()
 	{
